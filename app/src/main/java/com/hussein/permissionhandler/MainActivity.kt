922078085fb84edb49d6fe59b20da4bd20de49c6
husseinkamal <hussain@aqwas.sa>
2023-11-15 20:26:28 +0200

    permissions handler jetpack compose

---
+++ app/src/main/java/com/hussein/permissionhandler/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.hussein.permissionhandler
+
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.hussein.permissionhandler.ui.theme.PermissionHandlerTheme
+
+@ExperimentalPermissionsApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PermissionHandlerTheme {
+                val permission = rememberMultiplePermissionsState(permissions = listOf(
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.CAMERA,
+                ))
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PermissionHandlerTheme {
+        Greeting("Android")
+    }
+}
